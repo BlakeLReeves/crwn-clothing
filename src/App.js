a5db5404d7d0af327cfa408cae9469d266310718
blakereeves19 <blake.reeves19@gmail.com>
2021-02-16 22:29:07 -0600

    Connected and fetched shop data from firestore

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import ShopPage from './pages/shoppage/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
